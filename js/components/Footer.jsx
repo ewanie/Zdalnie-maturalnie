8269f1a08de28fde5fcdf38e3c5f01603898b7eb
Ewa Niezgoda <eniezgo@gmail.com>
2018-07-27 12:00:58 +0200

    scrolling fixed

--- js/components/Footer.jsx
+++ js/components/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends Component {
     render(){
         return <div className="footer">
             <span className="footer__copyright">&copy; Copyright by Zdalnie Maturalnie</span>
-            <span className="footer__created">made with &hearts; by moi</span>
+            <span className="footer__created">made with &hearts; by <a href="mailto:eniezgo@gmail.com">evix</a></span>
         </div>
     }
 }
--- js/components/Menu.jsx
+++ js/components/Menu.jsx
@@ -6,13 +6,15 @@ import $ from "jquery";
 $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
-            event.preventDefault();
             var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function(){
-                window.location.hash = hash;
-            });
+            if($(hash).offset()){
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 1000, function(){
+                    window.location.hash = hash;
+                });
+            }
         }
     });
 });
